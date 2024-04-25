caaf4857d9e70a9bcbec31031650be1b302c6cf6
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-24 20:13:09 -0500

    feat: :sparkles: add main section

---
+++ src/sections/Main.tsx
@@ -0,0 +1,131 @@
+export default function Main () {
+  return (
+    <main>
+      <h2 className="text-orange-600 text-3xl md:text-5xl w-full text-center py-8 font-bold">
+        Nuestra Colección
+      </h2>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8 py-8">
+        <div className="grid grid-cols-3 px-4 gap-4">
+          <div className="col-span-1">
+            <img src="/img/guitarra_01.jpg" alt="guitarra 01" />
+          </div>
+          <div className="col-span-2 flex flex-col justify-between">
+            <h4 className="font-medium uppercase text-2xl">
+              Lukather
+            </h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ipsam nihil molestias fugiat totam explicabo quisquam
+            </p>
+            <p className="text-orange-600 font-semibold text-3xl">
+              $299
+            </p>
+            <button className="w-full bg-black text-white uppercase text-center text-lg font-semibold py-2">
+              Agregar al carrito
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 px-4 gap-4">
+          <div className="col-span-1">
+            <img src="/img/guitarra_01.jpg" alt="guitarra 01" />
+          </div>
+          <div className="col-span-2 flex flex-col justify-between">
+            <h4 className="font-medium uppercase text-2xl">
+              Lukather
+            </h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ipsam nihil molestias fugiat totam explicabo quisquam
+            </p>
+            <p className="text-orange-600 font-semibold text-3xl">
+              $299
+            </p>
+            <button className="w-full bg-black text-white uppercase text-center text-lg font-semibold py-2">
+              Agregar al carrito
+            </button>
+          </div>
+        </div>
+        
+        <div className="grid grid-cols-3 px-4 gap-4">
+          <div className="col-span-1">
+            <img src="/img/guitarra_01.jpg" alt="guitarra 01" />
+          </div>
+          <div className="col-span-2 flex flex-col justify-between">
+            <h4 className="font-medium uppercase text-2xl">
+              Lukather
+            </h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ipsam nihil molestias fugiat totam explicabo quisquam
+            </p>
+            <p className="text-orange-600 font-semibold text-3xl">
+              $299
+            </p>
+            <button className="w-full bg-black text-white uppercase text-center text-lg font-semibold py-2">
+              Agregar al carrito
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 px-4 gap-4">
+          <div className="col-span-1">
+            <img src="/img/guitarra_01.jpg" alt="guitarra 01" />
+          </div>
+          <div className="col-span-2 flex flex-col justify-between">
+            <h4 className="font-medium uppercase text-2xl">
+              Lukather
+            </h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ipsam nihil molestias fugiat totam explicabo quisquam
+            </p>
+            <p className="text-orange-600 font-semibold text-3xl">
+              $299
+            </p>
+            <button className="w-full bg-black text-white uppercase text-center text-lg font-semibold py-2">
+              Agregar al carrito
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 px-4 gap-4">
+          <div className="col-span-1">
+            <img src="/img/guitarra_01.jpg" alt="guitarra 01" />
+          </div>
+          <div className="col-span-2 flex flex-col justify-between">
+            <h4 className="font-medium uppercase text-2xl">
+              Lukather
+            </h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ipsam nihil molestias fugiat totam explicabo quisquam
+            </p>
+            <p className="text-orange-600 font-semibold text-3xl">
+              $299
+            </p>
+            <button className="w-full bg-black text-white uppercase text-center text-lg font-semibold py-2">
+              Agregar al carrito
+            </button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 px-4 gap-4">
+          <div className="col-span-1">
+            <img src="/img/guitarra_01.jpg" alt="guitarra 01" />
+          </div>
+          <div className="col-span-2 flex flex-col justify-between">
+            <h4 className="font-medium uppercase text-2xl">
+              Lukather
+            </h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius ipsam nihil molestias fugiat totam explicabo quisquam
+            </p>
+            <p className="text-orange-600 font-semibold text-3xl">
+              $299
+            </p>
+            <button className="w-full bg-black text-white uppercase text-center text-lg font-semibold py-2">
+              Agregar al carrito
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
