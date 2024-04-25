20496f2e052cfcce7b41ff666203a065523a89e5
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-04-24 19:30:45 -0500

    feat: :sparkles: add tooltip to shopping cart

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,10 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
 export default function Header () {
   return (
     <section className="w-full bg-[url(/img/header.jpg)] bg-center bg-no-repeat bg-cover">
@@ -6,9 +13,127 @@ export default function Header () {
           <img src="/img/logo.svg" alt="logo de GuitarLA"
             className="w-48"
             />
-          <img src="/img/carrito.png" alt="carrito de compras" 
-            className="w-6"
-            />
+          
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger className="">
+                <img src="/img/carrito.png" alt="carrito de compras" 
+                  className="w-6"
+                />
+              </TooltipTrigger>
+              <TooltipContent side="bottom" align="end">
+                <div className="bg-white w-96 ">
+                  <table className="w-full mb-4">
+                    <thead>
+                      <tr className="border-b border-gray-300 pb-2">
+                        <th>Imagen</th>
+                        <th>Nombre</th>
+                        <th>Precio</th>
+                        <th>Cantidad</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody className="">
+                      <tr className="border-b border-gray-300">
+                        <td className="">
+                          <img src="/img/guitarra_01.jpg" alt="guitarra01" className="w-8 mx-auto"/>
+                        </td>
+                        <td className="text-center">
+                          Lukather
+                        </td>
+                        <td className="text-center font-semibold">
+                          $299
+                        </td>
+                        <td className="text-center space-x-2">
+                          <span className="p-0 w-4 h-6 font-semibold rounded-md inline-flex justify-center items-center bg-black text-white">
+                            +
+                          </span>
+                          <span>
+                            1
+                          </span>
+                          <span className="p-0 w-4 h-6 font-semibold rounded-md inline-flex justify-center items-center bg-black text-white">
+                            -
+                          </span>
+                        </td>
+                        <td className="text-center">
+                          <div className="bg-red-500 text-white rounded-full p-1 font-semibold">
+                            x
+                          </div>
+                        </td>
+                      </tr>
+
+                      <tr className="border-b border-gray-300">
+                        <td className="">
+                          <img src="/img/guitarra_01.jpg" alt="guitarra01" className="w-8 mx-auto"/>
+                        </td>
+                        <td className="text-center">
+                          Lukather
+                        </td>
+                        <td className="text-center font-semibold">
+                          $299
+                        </td>
+                        <td className="text-center space-x-2">
+                          <span className="p-0 w-4 h-6 font-semibold rounded-md inline-flex justify-center items-center bg-black text-white">
+                            +
+                          </span>
+                          <span>
+                            1
+                          </span>
+                          <span className="p-0 w-4 h-6 font-semibold rounded-md inline-flex justify-center items-center bg-black text-white">
+                            -
+                          </span>
+                        </td>
+                        <td className="text-center">
+                          <div className="bg-red-500 text-white rounded-full p-1 font-semibold">
+                            x
+                          </div>
+                        </td>
+                      </tr>
+
+                      <tr className="border-b border-gray-300">
+                        <td className="">
+                          <img src="/img/guitarra_01.jpg" alt="guitarra01" className="w-8 mx-auto"/>
+                        </td>
+                        <td className="text-center">
+                          Lukather
+                        </td>
+                        <td className="text-center font-semibold">
+                          $299
+                        </td>
+                        <td className="text-center space-x-2">
+                          <span className="p-0 w-4 h-6 font-semibold rounded-md inline-flex justify-center items-center bg-black text-white">
+                            +
+                          </span>
+                          <span>
+                            1
+                          </span>
+                          <span className="p-0 w-4 h-6 font-semibold rounded-md inline-flex justify-center items-center bg-black text-white">
+                            -
+                          </span>
+                        </td>
+                        <td className="text-center">
+                          <div className="bg-red-500 text-white rounded-full p-1 font-semibold">
+                            x
+                          </div>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+
+                  <div className="text-right mb-4">
+                    Total pagar:
+                    <span className="font-semibold">
+                      $1276
+                    </span>
+                  </div>
+
+                  <button className="uppercase w-full px-4 py-2 text-center text-white bg-black rounded-md font-semibold">
+                    Vaciar carrito
+                  </button>
+                </div>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </header>
       </div>
     </section>
