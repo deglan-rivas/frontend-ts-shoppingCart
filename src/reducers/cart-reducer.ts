21aa62ffa984e1d4a29ebf5ceffc71813370e8ef
deglan rivas <deglan.rivas@pucp.edu.pe>
2024-05-30 18:05:29 -0500

    feat: :sparkles: add cart reducer

---
+++ src/reducers/cart-reducer.ts
@@ -0,0 +1,48 @@
+import { CartItem } from "@/types";
+
+type CartState = {
+  cart: CartItem[]
+}
+
+type CartAction =
+  { type: "add-to-cart", payload: { item: CartItem } } |
+  { type: "increase-quantity", payload: { id: CartItem["id"] } } |
+  { type: "decrease-quantity", payload: { id: CartItem["id"] } } |
+  { type: "delete-cart-item", payload: { id: CartItem["id"] } } |
+  { type: "clean-cart" }
+
+
+export const initialState: CartState = {
+  cart: []
+}
+
+export const cartReducer = (
+  state: CartState = initialState,
+  action: CartAction
+) => {
+
+  switch (action.type) {
+    case "add-to-cart":
+      return {
+        ...state
+      }
+    case "increase-quantity":
+      return {
+        ...state
+      }
+    case "decrease-quantity":
+      return {
+        ...state
+      }
+    case "delete-cart-item":
+      return {
+        ...state
+      }
+    case "clean-cart":
+      return {
+        ...state
+      }
+    default:
+      return state
+  }
+}
